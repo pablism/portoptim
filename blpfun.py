3273042a6a3ae090a56d4bde9b84c08a958b040b
Pablo Mendez <pablo.mendezm@proteccion.com.co>
2023-11-07 16:19:26 -0500

    version muchas

---
+++ blpfun.py
@@ -0,0 +1,140 @@
+import blpapi
+import pandas as pd
+import numpy as np
+
+def bdh(tickers,fld,dts):
+    #tickers must be a list
+    #fld is a String with bloomberg field (only 1 fld at a time is accepted)
+    #dts is a tuple with start and end date
+    sessionOptions = blpapi.SessionOptions()
+    session = blpapi.Session(sessionOptions)
+    session.start()
+    session.openService("//blp/refdata")
+    refDataService = session.getService("//blp/refdata")
+    d1 = dts[0]
+    d2 = dts[1]
+    request = refDataService.createRequest("HistoricalDataRequest")
+    for t in tickers:
+        request.getElement('securities').appendValue(t)
+    request.getElement("fields").appendValue(fld)
+    request.set("startDate", str(d1))
+    request.set("endDate", str(d2))
+    request.set("maxDataPoints", 100000)
+    session.sendRequest(request)
+    df=[]
+    while(True):
+        ev = session.nextEvent(500)
+        for msg in ev:
+            if msg.messageType() == 'HistoricalDataResponse':
+                sec = msg.getElement('securityData').getElementAsString('security')
+                for s in msg.asElement().getChoice().elements():
+                    if s.name() =='fieldData':
+                        dt=[]
+                        vl=[]
+                        for i in s.values():
+                            #print(i.getElementAsFloat('PX_LAST'))
+                            vl.append(i.getElementAsFloat(fld))
+                            dt.append(i.getElementAsDatetime('date'))
+                            #print(i.getElementAsDatetime('date'))
+                        df.append(pd.DataFrame([dt,vl],index=['date',sec]).transpose().set_index('date'))
+
+        if ev.eventType() == blpapi.Event.RESPONSE:
+                break
+    session.stop()
+    df = pd.concat(df,axis=1)
+    df.index = pd.to_datetime(df.index)
+    df = df.sort_index()
+    return df
+
+def bdp(tickers,fld,orides={},valtype='float'):
+    #tickers must be a list
+    #fld is a String with bloomberg field (only 1 fld at a time is accepted)
+    #orides is a dictionary of override fields. {'override','value}. (OPTIONAL)
+    #valtype is the type of value expected , (OPTIONAL, defaults to float)
+    
+    sessionOptions = blpapi.SessionOptions()
+    session = blpapi.Session(sessionOptions)
+    session.start()
+    session.openService("//blp/refdata")
+    refDataService = session.getService("//blp/refdata")
+    request = refDataService.createRequest("ReferenceDataRequest")
+    for t in tickers:
+        request.getElement('securities').appendValue(t)
+    request.getElement("fields").appendValue(fld)
+    if bool(orides):
+        for k,v in orides.items():
+            h = request.getElement('overrides').appendElement()
+            h.setElement('fieldId',k)
+            h.setElement('value',v)
+    
+    session.sendRequest(request)
+    dct={}
+    while(True):
+        ev = session.nextEvent(500)
+        for msg in ev:
+            if msg.messageType() == 'ReferenceDataResponse':
+                for v in msg.getElement('securityData').values():
+                    sec = v.getElementAsString('security')
+                    if valtype=='str':
+                        try:
+                            val = v.getElement('fieldData').getElementAsString(fld)
+                        except:val=np.nan
+                    elif valtype=='float':
+                        try:
+                            val = v.getElement('fieldData').getElementAsFloat(fld)
+                        except: val=np.nan
+                        
+                    dct[sec]=val
+        if ev.eventType() == blpapi.Event.RESPONSE:
+                break
+    session.stop()
+    return dct
+
+def bdp_df(tickers,flds,orides={}):
+    labels_ =['ticker']
+    labels_.extend(flds)
+
+    sessionOptions = blpapi.SessionOptions()
+    session = blpapi.Session(sessionOptions)
+    session.start()
+    session.openService("//blp/refdata")
+    refDataService = session.getService("//blp/refdata")
+    request = refDataService.createRequest("ReferenceDataRequest")
+    for t in tickers:
+        request.getElement('securities').appendValue(t)
+    for f in flds:
+        request.getElement("fields").appendValue(f)
+    if bool(orides):
+        for k,v in orides.items():
+            h = request.getElement('overrides').appendElement()
+            h.setElement('fieldId',k)
+            h.setElement('value',v)
+
+    
+    session.sendRequest(request)
+    rows_=[]
+    while(True):
+        ev = session.nextEvent(500)
+        for msg in ev:
+            if msg.messageType() == 'ReferenceDataResponse':
+
+                for v in msg.getElement('securityData').values():
+                    sec = v.getElementAsString('security')
+                    val = v.getElement('fieldData')
+                    c = []
+                    c.append(sec)
+                    for f in flds:
+                        try:
+                            c.append(val[f])
+                        except:
+                            c.append(np.nan)
+                    rows_.append(c)
+
+       
+        if ev.eventType() == blpapi.Event.RESPONSE:
+                break
+    session.stop()
+    df = pd.DataFrame(rows_,columns=labels_)
+    df.set_index('ticker',inplace=True)
+
+    return df
--- funciones_optim.py
+++ funciones_optim.py
@@ -1,8 +1,12 @@
+from tkinter import W
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize, basinhopping
 from itertools import product
 from numpy.linalg import cholesky
+from mystuff import bdh
+
+#optim
 
 def port_ret(w,r):
     return -np.array(r).dot(w)
@@ -26,3 +30,66 @@ def min_vol(bounds,cov,w):
     GAVP = minimize(var,w,args=(cov,0),bounds=bounds,constraints=constraints)
     minv = np.sqrt(var(GAVP.x,cov,0))
     return minv
+
+
+#other
+
+def normi(x,r):
+    m = x.rolling(r,min_periods=1).mean()
+    s = x.rolling(r,min_periods=1).std()
+    z = x.subtract(m).div(s)
+    return z
+
+def momm(x,mas,mal):
+    z = x.rolling(mas,min_periods=1).sum()
+    s = z.rolling(mal).std()
+
+    return z.div(s)
+
+def prat(pr,wds):
+    ratios = []
+    for wd in wds:
+        base_ = pr.rolling(wd).max() - pr.rolling(wd).min()
+        level = pr - pr.rolling(wd).min()
+        x = level.div(base_)
+        ratios.append(x)
+    ratios = pd.concat(ratios,axis=1)
+    ratios.columns = [f'{pr.name}_{i}' for i in wds]
+    return ratios
+
+class _data:
+    def __init__(self):
+        refs = pd.read_csv('tickers.csv',index_col='ticker')
+        ticks = refs.label.to_dict()
+        data = bdh(ticks.keys(),'px_last',(20101231,20231026))
+        data.columns = pd.MultiIndex.from_tuples([(refs.loc[c,'label'],refs.loc[c,'type']) for c in data.columns],names=('label','typ'))
+        data.index = pd.to_datetime(data.index)
+        dataw = data.resample('W-Wed').last().ffill().dropna(axis=0)
+        x_ = dataw.loc[:,[c[1] in ['MACRO','SENTIMENT','POSITIONING','MARKETIMPLIED'] for c in dataw.columns]].droplevel(level=1,axis=1)
+        x_['AAII'] = x_.BULL.subtract(x_.BEAR)
+        x_.drop(['BULL','BEAR'],axis=1,inplace=True)
+        x_ = x_.apply(normi,axis=0,args=[260])
+        rtw_ = dataw.xs('ASSETCLASS',level='typ',axis=1).drop(['USCASH','COCASH'],axis=1).dropna(axis=0)
+
+        rtw = rtw_.pct_change()
+        rtw.USTTEN = rtw_.USTTEN.diff()
+        
+        pt_ = data.xs('ASSETCLASS',level='typ',axis=1).drop(['USCASH','COCASH'],axis=1).dropna(axis=0)
+        rtd = pt_.pct_change()
+        rtd.USTTEN = pt_.USTTEN.diff()
+        prat_ = pd.concat([prat(pt_.loc[:,c],[4,8,16,32,64]) for c in pt_.columns],axis=1).dropna(axis=0)
+
+        self.data = data
+        self.macrox = x_
+        self.prat = prat_ 
+        self.rtw = rtw
+        self.rtd = rtd
+
+    def set_labels(self,w):
+        def signal(rtw,w):
+            y = rtw.rolling(w).sum()
+            y.loc[y>0] = 1
+            y.loc[y<0] = 0
+            y = y.shift(-w)
+            return y
+        self.ylabels = self.rtd.apply(signal,args=[w],axis=0).dropna(axis=0)
