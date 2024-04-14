9b11935d01b9ed3f8a1d5e79409d56c7be8d7e94
PEM <pablomendezm@gmail.com>
2024-04-14 01:11:19 -0500

    version 1 backtest

--- funciones_optim.py
+++ funciones_optim.py
@@ -5,6 +5,7 @@ from scipy.optimize import minimize, basinhopping
 from itertools import product
 from numpy.linalg import cholesky
 from mystuff import bdh
+import datetime
 
 #optim
 
@@ -93,3 +94,89 @@ class _data:
             y = y.shift(-w)
             return y
         self.ylabels = self.rtd.apply(signal,args=[w],axis=0).dropna(axis=0)
+
+
+class backt :
+
+    def loadprices(sdate=20191231):
+        tk = pd.read_csv('tickers.csv')
+        tk = tk.loc[tk['type']=='ASSETCLASS'].set_index('ticker').label.to_dict()
+        p = bdh(tk.keys(),'PX_LAST',(sdate,datetime.datetime.today().strftime('%Y%m%d')))
+        p.index = pd.to_datetime(p.index)
+        p = p.sort_index()
+        p.rename(columns=tk,inplace=True)
+        p = p.reindex(pd.date_range(p.index[0],p.index[-1])).ffill()
+
+        return p
+
+
+    def rt(x,p):
+        if x.name in ['USDCOP']:
+            r = x.pct_change(fill_method=None)+(p.loc[x.index,'USCASH']-p.loc[x.index,'COCASH'])/36500
+        elif x.name in ['COCASH','USCASH']:
+            r = x.div(100).div(365)
+        elif x.name in ['USTTEN']:
+            r = -9*x.div(100).diff() + x.div(100).div(365)
+        else:
+            r = x.pct_change(fill_method=None)
+        return r
+
+    def portr(r,sdate,fdate,w,rebal=True,rbw=None):
+        dts = pd.date_range(sdate,fdate)
+        wp = []
+        rp = []
+        wp.append(w)
+        rp.append(0)
+        for i,d in enumerate(dts):
+            if rebal:
+                if d in rbw.index:
+                    wp.append(rbw.loc[d,:].values)
+                else:
+                    wp.append(np.multiply(wp[i],r.loc[d,:].add(1))/(np.dot(wp[i],r.loc[d,:])+1))
+            else:
+                wp.append(np.multiply(wp[i],r.loc[d,:].add(1))/(np.dot(wp[i],r.loc[d,:])+1))
+            rp.append(np.dot(wp[i],r.loc[d,:]))
+
+        wp = pd.DataFrame(wp[1:],index=dts)
+        rp = pd.Series(rp[1:],index=dts)
+        return wp, rp
+        
+    def mdd(x):
+        dd = x.copy()
+        mx = dd.iloc[0]
+        dd.iloc[0] = 0
+        for i, r in enumerate(x.index):
+            if i > 0:
+                if x.iloc[i] >= mx:
+                    mx = x.iloc[i]
+                    dd.iloc[i] = 0
+                else:
+                    dd.iloc[i] = x.iloc[i] / mx - 1
+        return min(dd)
+
+    def pmetrics(ports,cash,bench=0):
+        ret = ports.add(1).apply(np.log).sum().apply(np.exp).subtract(1).mul(100)
+        retm = ports.add(1).apply(np.log).resample('M').sum().apply(np.exp).subtract(1).mean().mul(100)
+        volm = ports.add(1).apply(np.log).resample('W').sum().apply(np.exp).subtract(1).std().mul(np.sqrt(4)).mul(100)
+        # alpha = ret - ports.loc[:,bench].add(1).add(1).apply(np.log).resample('Q').sum().apply(np.exp).subtract(1).mean()
+        # alphaq = retq - alpha.resample('Q').mean()
+        sharpe = (retm - cash.loc[ports.index].add(1).apply(np.log).resample('M').sum().apply(np.exp).subtract(1).mean()*100)/volm
+        b100 = ports.add(1).apply(np.log).cumsum().apply(np.exp).mul(100)
+        md = b100.apply(backt.mdd,axis=0).mul(100)
+        retms = ports.add(1).apply(np.log).resample('M').sum().apply(np.exp).subtract(1)
+        etl  = pd.Series([retms.loc[retms.loc[:,c]<=retms.loc[:,c].quantile(0.05),c].mean() for c in retms.columns],index=ports.columns)
+        etg  = pd.Series([retms.loc[retms.loc[:,c]>=retms.loc[:,c].quantile(0.95),c].mean() for c in retms.columns],index=ports.columns)
+        etgetl = etg.abs()/etl.abs()
+        downdev = pd.Series([np.sqrt((retms.loc[retms.loc[:,c]<=0,c]**2).sum()/len(retms.loc[retms.loc[:,c]<=0,c]))*100 for c in retms.columns],index=ports.columns)
+        sortino = retm.div(downdev)
+        df = pd.DataFrame({
+            'ret':ret,
+            'retm':retm,
+            'volm':volm,
+            'sharpe':sharpe,
+            'mdd':md,
+            'etg/etl':etgetl,
+            'downdev':downdev,
+            'sortino':sortino
+            },index=ports.columns)
+        return df.transpose()
